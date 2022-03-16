4b27664d0e99126e2654cb37ed4afd1c1476365f
Richard Solomou <richard@solomou.io>
2022-03-16 10:24:30 +0200

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import dynamic from "next/dynamic";
+
+const Canvas = dynamic(() => import("../components/Canvas"), { ssr: false });
+
+const Home: NextPage = () => {
+  return <Canvas />;
+};
+
+export default Home;
