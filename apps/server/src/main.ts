4ceaee54763c17fab0664201e7c85371ddc47a85
Richard Solomou <richard@solomou.io>
2022-03-31 00:30:49 +0300

    add swagger for API documentation

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Tarrasque.io Canary API')
+    .setDescription('Mobile-friendly virtual tabletop for Dungeons & Dragons')
+    .setVersion('1.0')
+    .addTag('cats')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3001);
 }
 bootstrap();
