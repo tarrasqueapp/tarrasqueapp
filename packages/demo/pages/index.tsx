cbf8386ea5d3b110e57b27b636f9b5bf4e8bb0d3
Richard Solomou <richard@solomou.io>
2022-07-26 02:36:03 +0300

    try with request-ip

--- packages/demo/pages/index.tsx
+++ packages/demo/pages/index.tsx
@@ -6,12 +6,21 @@ const Index: NextPage<any> = ({ ips }) => {
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  const requestIp = require('request-ip');
+  const ip = requestIp.getClientIp(context.req);
+
   const ips = [
+    context.req.headers['X-Client-IP'],
     context.req.headers['X-Forwarded-For'],
-    context.req.headers['X-Real-Ip'],
     context.req.headers['CF-Connecting-IP'],
+    context.req.headers['True-Client-Ip'],
+    context.req.headers['X-Real-IP'],
+    context.req.headers['X-Forwarded'],
+    context.req.headers['Forwarded-For'],
     context.req.connection.remoteAddress,
     context.req.socket.remoteAddress,
+    context.req.headers['Cf-Pseudo-IPv4'],
+    ip,
   ];
 
   return { props: { ips } };
