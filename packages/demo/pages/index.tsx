11a69e32852bb0d07a33f9d9cb651d4ed332cbee
Richard Solomou <richard@solomou.io>
2022-07-25 23:11:29 +0300

    cloudflare proxy

--- packages/demo/pages/index.tsx
+++ packages/demo/pages/index.tsx
@@ -1,20 +1,21 @@
 import type { GetServerSideProps, NextPage } from 'next';
 import React from 'react';
 
-const Index: NextPage<{ ip1: string; ip2: string; ip3: string }> = ({ ip1, ip2, ip3 }) => {
+const Index: NextPage<any> = ({ ip1, ip2, ip3, ip4 }) => {
   return (
     <>
-      It works! Your IP is {JSON.stringify(ip1)} {JSON.stringify(ip2)} {JSON.stringify(ip3)}
+      It works! Your IP is {JSON.stringify(ip1)} {JSON.stringify(ip2)} {JSON.stringify(ip3)} {JSON.stringify(ip4)}
     </>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const ip1 = context.req.headers['x-forwarded-for'];
-  const ip2 = context.req.headers['x-real-ip'];
-  const ip3 = context.req.connection.remoteAddress;
+  const ip1 = context.req.headers['X-Forwarded-For'];
+  const ip2 = context.req.headers['X-Real-Ip'];
+  const ip3 = context.req.headers['CF-Connecting-IP'];
+  const ip4 = context.req.connection.remoteAddress;
 
-  return { props: { ip1, ip2, ip3 } };
+  return { props: { ip1, ip2, ip3, ip4 } };
 };
 
 export default Index;
