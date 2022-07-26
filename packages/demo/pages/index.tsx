39730da5255e8d02562552371b79120fd5fe0348
Richard Solomou <richard@solomou.io>
2022-07-26 08:20:17 +0300

    return single ip

--- packages/demo/pages/index.tsx
+++ packages/demo/pages/index.tsx
@@ -1,29 +1,15 @@
 import type { GetServerSideProps, NextPage } from 'next';
 import React from 'react';
 
-const Index: NextPage<any> = ({ ips }) => {
-  return <>It works! Your IP is {JSON.stringify(ips)}</>;
+const Index: NextPage<{ ip: string }> = ({ ip }) => {
+  return <>It works! Your IP is {ip}</>;
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const requestIp = require('request-ip');
   const ip = requestIp.getClientIp(context.req);
 
-  const ips = [
-    context.req.headers['X-Client-IP'],
-    context.req.headers['X-Forwarded-For'],
-    context.req.headers['CF-Connecting-IP'],
-    context.req.headers['True-Client-Ip'],
-    context.req.headers['X-Real-IP'],
-    context.req.headers['X-Forwarded'],
-    context.req.headers['Forwarded-For'],
-    context.req.connection.remoteAddress,
-    context.req.socket.remoteAddress,
-    context.req.headers['Cf-Pseudo-IPv4'],
-    ip,
-  ];
-
-  return { props: { ips } };
+  return { props: { ip } };
 };
 
 export default Index;
