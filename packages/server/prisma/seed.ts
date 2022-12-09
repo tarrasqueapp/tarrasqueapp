022b19658af616fff4eeaaf0f459261bca448c29
Richard Solomou <richard@solomou.io>
2022-12-10 00:50:58 +0200

    update seed script

--- packages/server/prisma/seed.ts
+++ packages/server/prisma/seed.ts
@@ -1,62 +1,93 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Role } from '@prisma/client';
+import argon2 from 'argon2';
+import cuid from 'cuid';
+
+import { SetupStep } from '../src/setup/setup-step.enum';
 
 const prisma = new PrismaClient();
 
 async function main() {
   try {
-    console.debug('📂 Deleting all media...');
-    await prisma.media.deleteMany({});
-    console.debug('📂 Deleting all tokens...');
-    await prisma.token.deleteMany({});
-    console.debug('📂 Deleting all maps...');
-    await prisma.map.deleteMany({});
     console.debug('📂 Deleting all movement types...');
     await prisma.movement.deleteMany({});
+
     console.debug('📂 Deleting all hit points...');
     await prisma.hitPoints.deleteMany({});
+
     console.debug('📂 Deleting all armor classes...');
     await prisma.armorClass.deleteMany({});
+
     console.debug('📂 Deleting all senses...');
     await prisma.senses.deleteMany({});
-    console.debug('📂 Deleting all skills...');
-    await prisma.skill.deleteMany({});
+
     console.debug('📂 Deleting all abilities...');
     await prisma.ability.deleteMany({});
+
+    console.debug('📂 Deleting all skills...');
+    await prisma.skill.deleteMany({});
+
     console.debug('📂 Deleting all currencies...');
     await prisma.currencies.deleteMany({});
+
+    console.debug('📂 Deleting all media...');
+    await prisma.media.deleteMany({});
+
     console.debug('📂 Deleting all player characters...');
     await prisma.playerCharacter.deleteMany({});
+
     console.debug('📂 Deleting all non-player characters...');
     await prisma.nonPlayerCharacter.deleteMany({});
+
+    console.debug('📂 Deleting all tokens...');
+    await prisma.token.deleteMany({});
+
+    console.debug('📂 Deleting all maps...');
+    await prisma.map.deleteMany({});
+
     console.debug('📂 Deleting all campaigns...');
     await prisma.campaign.deleteMany({});
+
+    console.debug('📂 Deleting all refresh tokens...');
+    await prisma.refreshToken.deleteMany({});
+
     console.debug('📂 Deleting all plugins...');
     await prisma.plugin.deleteMany({});
+
     console.debug('📂 Deleting all users...');
     await prisma.user.deleteMany({});
+
+    console.debug('📂 Deleting all setup data...');
+    await prisma.setup.deleteMany({});
+
     console.debug('✅️ All data deleted.');
 
     console.debug('📂 Creating new user...');
+    const userId = cuid();
     await prisma.user.create({
       data: {
-        id: '1',
-        name: 'Example User',
-        email: 'example@example.com',
-        password: 'password',
+        id: userId,
+        name: 'Richard',
+        email: 'richard@tarrasque.app',
+        roles: [Role.USER, Role.ADMIN],
+        password: await argon2.hash('password'),
       },
     });
+
     console.debug('📂 Creating new campaign...');
+    const campaignId = cuid();
     await prisma.campaign.create({
       data: {
-        id: '1',
+        id: campaignId,
         name: 'Example Campaign',
-        createdBy: { connect: { id: '1' } },
+        createdBy: { connect: { id: userId } },
       },
     });
+
     console.debug('📂 Creating new map...');
+    const mapId = cuid();
     await prisma.map.create({
       data: {
-        id: '1',
+        id: mapId,
         name: 'Example Map',
         media: {
           create: {
@@ -66,17 +97,19 @@ async function main() {
             height: 1400,
             size: 1360000,
             format: 'image/webp',
-            createdBy: { connect: { id: '1' } },
+            createdBy: { connect: { id: userId } },
           },
         },
-        campaign: { connect: { id: '1' } },
-        createdBy: { connect: { id: '1' } },
+        campaign: { connect: { id: campaignId } },
+        createdBy: { connect: { id: userId } },
       },
     });
+
     console.debug('📂 Creating new player character...');
+    const playerCharacterId = cuid();
     await prisma.playerCharacter.create({
       data: {
-        id: '1',
+        id: playerCharacterId,
         name: 'Example Player Character',
         armorClass: { create: { description: 'Natural armor' } },
         hitPoints: { create: {} },
@@ -91,7 +124,7 @@ async function main() {
             height: 280,
             size: 15440,
             format: 'image/webp',
-            createdBy: { connect: { id: '1' } },
+            createdBy: { connect: { id: userId } },
           },
         },
         abilities: {
@@ -145,10 +178,11 @@ async function main() {
             },
           ],
         },
-        createdBy: { connect: { id: '1' } },
-        campaign: { connect: { id: '1' } },
+        createdBy: { connect: { id: userId } },
+        campaign: { connect: { id: campaignId } },
       },
     });
+
     console.debug('📂 Creating new token...');
     await prisma.token.create({
       data: {
@@ -156,11 +190,12 @@ async function main() {
         height: 40,
         x: 80,
         y: 80,
-        createdBy: { connect: { id: '1' } },
-        map: { connect: { id: '1' } },
-        playerCharacter: { connect: { id: '1' } },
+        createdBy: { connect: { id: userId } },
+        map: { connect: { id: mapId } },
+        playerCharacter: { connect: { id: playerCharacterId } },
       },
     });
+
     console.debug('📂 Creating new plugin...');
     await prisma.plugin.create({
       data: {
@@ -168,6 +203,16 @@ async function main() {
         url: 'https://github.com/tarrasqueapp/tarrasque-example-plugin',
       },
     });
+
+    console.debug('📂 Creating new setup data...');
+    await prisma.setup.create({
+      data: {
+        id: 1,
+        step: SetupStep.COMPLETED,
+        completed: true,
+      },
+    });
+
     console.debug('✅️ New data created.');
   } catch (error) {
     console.error(error);
