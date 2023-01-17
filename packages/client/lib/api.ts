fbaa96fd4294f8569346d7d6604a934021079271
Richard Solomou <richard@solomou.io>
2023-01-17 22:52:46 +0200

    add deploy pr for sandbox

--- packages/client/lib/api.ts
+++ packages/client/lib/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { config } from './config';
 
 export const api = axios.create({
-  baseURL: typeof window !== 'undefined' ? config.HOST : 'http://server:3000',
+  baseURL: typeof window !== 'undefined' ? config.HOST : config.AXIOS_BASE_URL,
 });
 
 // Always return a readable error message
