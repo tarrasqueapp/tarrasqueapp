fc54933024bc5a126ee2b72e19306918939455f1
Richard Solomou <richard@solomou.io>
2023-01-22 00:43:32 +0200

    update dockerfiles

--- apps/ui/next.config.js
+++ apps/ui/next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
   env: {
     // Global
     VERSION: process.env.VERSION,
